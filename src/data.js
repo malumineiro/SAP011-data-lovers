7f6bcda1862bac735efadce17ff1ccf86ca9aeae
Gabriella Vieira Bruno <gabi_bvieira@hotmail.com>
2023-08-14 12:32:42 -0300

    realizei a ordenação

--- src/data.js
+++ src/data.js
@@ -1,9 +1,11 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
+export const ordenarAaZ = (paises) => {
+  return paises.sort((a, b) => {
+    if (a.name.common > b.name.common) {
+      return 1;
+    }
+    if (a.name.common < b.name.common) {
+      return -1;
+    }
+    return 0;
+  });
 };
