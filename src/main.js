1df1262c2add32cc8d6bd50b0909ee6d484e634e
Maria Luiza Mineiro Batista <lubmineiro@hotmail.com>
2023-08-24 16:12:54 -0300

    main.js e inspecionar CSS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import paises from "./data/countries/countries.js";
 
+const btnCountries= document.querySelector("#btn-countries") 
+btnCountries.addEventListener("click",()=>{
+  const home= document.getElementById("home-welcome") 
+  home.classList.add("none")
+
+  const pageCountries= document.getElementById("page-countries")
+  pageCountries.classList.add("flex")
+
+})
+
+
 
 paises.countries.forEach((pais) => {
   const countriesConteiner = document.querySelector("#countries-conteiner");
@@ -20,22 +31,29 @@ paises.countries.forEach((pais) => {
   title.innerText = pais.name.common;
   card.appendChild(title);
 
-  // criação das linguagens
-  if (pais.languages) {
+   // criação da capital
+   const capital = document.createElement("span");
+   capital.classList.add("card-informations");
+   capital.innerText = `Capital: ${pais.capital}`;
+   card.appendChild(capital);
+
+  // criação da populaçãp 
+  const population = document.createElement("span");
+  population.classList.add("card-informations");
+  population.innerText = `População: ${pais.population.toLocaleString("pt-BR")}`;
+  card.appendChild(population);
+
+   // criação das linguagens
+   if (pais.languages) {
     const languages = document.createElement("span");
     languages.classList.add("card-informations");
-    languages.innerText = `linguagens: ${Object.values(pais.languages).join(
+    languages.innerText = `Idioma: ${Object.values(pais.languages).join(
       ", "
     )}`;
     card.appendChild(languages);
   }
 
-  // criação da capital
-  const capital = document.createElement("span");
-  capital.classList.add("card-informations");
-  capital.innerText = pais.capital;
-  card.appendChild(capital);
-
   // renderiza o card na tela
   countriesConteiner.appendChild(card);
 });
+
